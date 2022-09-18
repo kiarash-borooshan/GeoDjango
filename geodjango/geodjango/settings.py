2978156e557e3c2d98bda01390c0df927061efa8
kiarash.borooshan <kiarash.borooshan@gmail.com>
2022-09-18 15:27:05 +0430

    database changed in Database_change branch

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'geodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': "geodjango",
+        "USER": "geo",
     }
 }
 
